a0d33825dba600f72847351ee8dfcff1b1fe0af4
kalyanbc12345@gmail.com <kalyanbc12345@gmail.com>
2022-04-09 17:01:00 +0900

    update

--- Tracker.java
+++ Tracker.java
@@ -1,4 +1,4 @@
-package coronatracker;
+package sendreport;
 
 import java.io.IOException;
 import java.net.URI;
@@ -9,63 +9,35 @@ import java.util.HashMap;
 
 import org.json.JSONObject;
 
+/**
+ * データを取得するクラス
+ * @author kalyan_cr7
+ *20220321
+ */
 public class Tracker {
 
-	/*
-	 * 1.データの取得
-	 * 　※APIから日付と場所を指定しデータを取得する
-	 * 2.データの加工
-	 * 　Mapへ変換
-	 * 　前日と当日の感染者数を取り出し、当日何人増えたか計算する
-	 * 3.メール送信処理
-	 * 4.タスクスケジュールで毎日実行するように設定
-	 */
-//	public static void main(String[] args) throws IOException, InterruptedException {
-//		LocalDate date = LocalDate.now().minusDays(2);
-//		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyyMMdd");
-//		String formatedDate = myFormatObj.format(date);
-//		System.out.println(formatedDate);
-//
-//		
-//		Tracker tracker = new Tracker();
-//		System.out.println(tracker.getData(formatedDate, "神奈川県"));
-//		System.out.println(tracker.getMapDate(tracker.getData(formatedDate, "神奈川県")));
-//		String test = "こんにちは \r\n世界";
-//		System.out.println(test);
-//
-//		
-//	}
-
 	/**
-	 * @param str
-	 * @return
+	 * APIから取ってきたデータを整形する
+	 * @param str　APIから取ってきたデータ
+	 * @return　感染者数を場所別に取得しやすくしたデータ
 	 */
 	public HashMap<String, String> getMapDate(String str) {
+
 		HashMap<String, String> map = new HashMap<>();
+
 		JSONObject obj = new JSONObject(str).getJSONArray("itemList").getJSONObject(0);
+		// 感染者数
 		map.put("number", obj.getString("npatients"));
+		// 場所
 		map.put("place", obj.getString("name_jp"));
+
 		return map;
 	}
 
-//	public HashMap<String, String> getNumber(HttpResponse<String> response) throws JsonMappingException, JsonProcessingException {
-//		Map<String, String> map = new HashMap<>();
-//		JSONObject obj = new JSONObject(response.body()).getJSONArray("itemList").getJSONObject(0);
-//		ObjectMapper mapper = new ObjectMapper();
-//		System.out.println(obj.getString("npatients"));
-//		
-//		//map = mapper.readValue(response.body(), new TypeReference<Map<String,Object>>(){});
-//		//mapper.(obj, new TypeReference<Map<String,Object>>(){});
-//		mapper.writeValueAsString(obj);
-//		System.out.println(obj);
-//		map = mapper.readValue(obj, new TypeReference<Map<String,String>>(){});
-//			return (HashMap<String, String>) map;
-//	}
-
 	/**
-	 * @param date
-	 * @param place
-	 * @return
+	 * @param date 取得日付
+	 * @param place　取得場所
+	 * @return　APIから引っ張ってきたデータ（JSONで取得する）
 	 * @throws IOException
 	 * @throws InterruptedException
 	 */
